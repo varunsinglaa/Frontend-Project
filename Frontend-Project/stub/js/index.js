bdd8f3b38cc98621b95e7660415683e59b664db1
varun <varunsingla1999@gmail.com>
2019-11-17 22:26:17 +0530

    First commit

---
+++ Frontend-Project/stub/js/index.js
@@ -0,0 +1,16 @@
+function getAllPosts()
+{
+  window.location.href="./html/bloglist.html";
+}
+
+
+function openCreatePost()
+{
+  document.getElementById('newPostModal').style.display='block';
+}
+
+
+function onCloseNewPost()
+{
+  document.getElementById('newPostModal').style.display='none';
+}
